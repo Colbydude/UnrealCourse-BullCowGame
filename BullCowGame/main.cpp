10cd48ffe77c6fb1b950c9a8566362c59d02727d
Colby Terry <colbydude@gmail.com>
2018-05-11 15:19:02 -0700

    Lecture 21. Variables and cin for input.

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
 int main()
 {
+	// Introduce the game.
 	constexpr int WORD_LENGTH = 5;
 
-	cout << "Welcome to Bulls and Cows, a fun word game." << endl;
-	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?" << endl;
+	cout << "Welcome to Bulls and Cows, a fun word game.\n";
+	cout << "Can you guess the " << WORD_LENGTH << " letter isogram I'm thinking of?\n";
+	cout << endl;
 
+	// Get a guess from the player.
+	cout << "Enter your guess: ";
+
+	string Guess = "";
+	cin >> Guess;
+
+	// Repeat the guess back to them.
+	cout << "Your guess was: " << Guess << endl;
+
+	cout << endl;
 	return 0;
 }
